6a74de40e62bee0606dd76fc2b4c5464df6f00fe
Jaedon Visva <jaedon.visva2006@gmail.com>
2026-07-04 16:40:52 -0400

    Complete lecture 3 asynchronous programming exercises (planning to add more)

--- lecture_3_asynchronous_programming.js
+++ lecture_3_asynchronous_programming.js
@@ -14,24 +14,42 @@ const fs = require('fs');
 // 1) Read a file using a callback and return its contents through the callback.
 // Use Node-style callback: callback(error, data)
 function readTextFile(path, callback) {
-  // TODO
+  fs.readFile(path, callback)
 }
 
 // 2) Parse JSON safely inside a callback-style workflow.
 function parseJsonString(jsonString, callback) {
-  // TODO
+  try {
+    const data = JSON.parse(jsonString);
+    callback(null, data)
+  } catch (error) {
+    callback(error)
+  }
 }
 
 // 3) Validate a number asynchronously.
 // If input is not a number, callback with an Error.
 // Otherwise callback with double the number.
 function doubleAsync(value, callback) {
-  // TODO
+  if (typeof value === 'number') {
+    callback(null, value * 2)
+  } else {
+    callback(new Error('not a number'))
+  }
 }
 
 // 4) Prefer early return: complete this function so it stops on error.
 function processUser(user, callback) {
   // TODO: if user is missing, callback with Error('User required')
+  if (!user){
+    callback(new Error("User required"));
+    return;
+  }
+  if (!user.name){
+    callback(new Error("Name required"));
+    return;
+  }
+  callback(null, user.name.toUpperCase())
   // TODO: if user.name is missing, callback with Error('Name required')
   // TODO: otherwise callback(null, user.name.toUpperCase())
 }
